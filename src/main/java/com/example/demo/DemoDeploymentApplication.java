6315c63fc1296c5511a551c0c64376bd2dc5bcbc
balajiPS <palaniswamybalaji64@gmail.com>
2024-05-10 21:38:38 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDeploymentApplication.class, args);
+	}
+
+}
